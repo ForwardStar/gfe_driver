c544ff2df4738804c3d2d184068060131abf78be
ForwardStar <119010351@link.cuhk.edu.cn>
2025-10-13 17:08:30 +0700

    Update scripts

--- scripts/run_analytics.sh
+++ scripts/run_analytics.sh
@@ -29,10 +29,10 @@ if [ -n "$1" ]; then
             ./optimizer datasets/com-lj.ungraph.v 8 > settings
         fi
         ./build/gfe_driver -G datasets/com-lj.ungraph.properties -u -l $1 -w $2 -r $2 -R $3 --blacklist cdlp > results/$1/analytics/com-lj-analytics-$1
-        # if [ "$1" = "radixgraph" ]; then
-        #     ./optimizer datasets/twitter-2010.v 8 > settings
-        # fi
-        # ./build/gfe_driver -G datasets/twitter-2010.properties -u -l $1 -w $2 -r $2 -R $3 --blacklist cdlp > results/$1/analytics/twitter-2010-analytics-$1
+        if [ "$1" = "radixgraph" ]; then
+            ./optimizer datasets/twitter-2010.v 8 > settings
+        fi
+        ./build/gfe_driver -G datasets/twitter-2010.properties -u -l $1 -w $2 -r $2 -R $3 --blacklist cdlp > results/$1/analytics/twitter-2010-analytics-$1
     elif [ -n "$2" ]; then
         if [ "$1" = "radixgraph" ]; then
             ./optimizer datasets/graph500-24.v 8 > settings
@@ -54,10 +54,10 @@ if [ -n "$1" ]; then
             ./optimizer datasets/com-lj.ungraph.v 8 > settings
         fi
         ./build/gfe_driver -G datasets/com-lj.ungraph.properties -u -l $1 -w $2 -r $2 -R 5 --blacklist cdlp > results/$1/analytics/com-lj-analytics-$1
-        # if [ "$1" = "radixgraph" ]; then
-        #     ./optimizer datasets/twitter-2010.v 8 > settings
-        # fi
-        # ./build/gfe_driver -G datasets/twitter-2010.properties -u -l $1 -w $2 -r $2 -R 5 --blacklist cdlp > results/$1/analytics/twitter-2010-analytics-$1
+        if [ "$1" = "radixgraph" ]; then
+            ./optimizer datasets/twitter-2010.v 8 > settings
+        fi
+        ./build/gfe_driver -G datasets/twitter-2010.properties -u -l $1 -w $2 -r $2 -R 5 --blacklist cdlp > results/$1/analytics/twitter-2010-analytics-$1
     else
         if [ "$1" = "radixgraph" ]; then
             ./optimizer datasets/graph500-24.v 8 > settings
@@ -79,10 +79,10 @@ if [ -n "$1" ]; then
             ./optimizer datasets/com-lj.ungraph.v 8 > settings
         fi
         ./build/gfe_driver -G datasets/com-lj.ungraph.properties -u -l $1 -w $(nproc) -r $(nproc) -R 5 --blacklist cdlp > results/$1/analytics/com-lj-analytics-$1
-        # if [ "$1" = "radixgraph" ]; then
-        #     ./optimizer datasets/twitter-2010.v 8 > settings
-        # fi
-        # ./build/gfe_driver -G datasets/twitter-2010.properties -u -l $1 -w $(nproc) -r $(nproc) -R 5 --blacklist cdlp > results/$1/analytics/twitter-2010-analytics-$1
+        if [ "$1" = "radixgraph" ]; then
+            ./optimizer datasets/twitter-2010.v 8 > settings
+        fi
+        ./build/gfe_driver -G datasets/twitter-2010.properties -u -l $1 -w $(nproc) -r $(nproc) -R 5 --blacklist cdlp > results/$1/analytics/twitter-2010-analytics-$1
     fi
     if [ "$1" = "radixgraph" ]; then
         rm settings
